e82de0f13a690b61bee9b8ad6d6c87a735159cf1
Eduard Jacobs <edoter92@gmail.com>
2021-06-28 22:00:26 -0700

    singleton pattern

---
+++ src/Singleton.ts
@@ -0,0 +1,20 @@
+// Only one instace will be created, which will Points back to itself
+
+class Singleton {
+    static instance: Singleton;
+    id: number;
+
+    constructor(id: number) {
+        this.id = id;
+        if(Singleton.instance){
+            return Singleton.instance;
+        }
+        Singleton.instance = this;
+    }
+}
+
+const singleton1 = new Singleton(1);
+const singleton2 = new Singleton(2);
+
+console.log('--', singleton1.id);
+console.log('--', singleton2.id)
