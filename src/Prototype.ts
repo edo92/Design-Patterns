e93e9487eb3bc804263fc2814a182721e9190131
Eduard Jacobs <edoter92@gmail.com>
2021-06-28 21:05:18 -0700

    basic prototype pattern

---
+++ src/Prototype.ts
@@ -0,0 +1,21 @@
+interface IProtoType {
+    clone();
+}
+
+class MyClassX implements IProtoType {
+    field: number[];
+
+    constructor(field: number[]) {
+        this.field = field;
+    }
+
+    public clone() {
+        return Object.assign({}, this); // Shellow Copy
+        // return JSON.parse(JSON.stringify(this)); // Deep Copy
+    }
+}
+
+const obj1 = new MyClassX([1, 2, 3, 4]);
+obj1.clone()
+
+console.log(obj1.field)
